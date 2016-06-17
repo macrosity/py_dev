f2dc150273485103e84510eba34b4ccb5e4c93ec
Ste Rose <macrosity@gmail.com>
2016-06-17 13:17:56 +0100

    Added code for exercise 1

--- ex1.py
+++ ex1.py
@@ -1,1 +1,7 @@
-This is a test file to see if I can clone it remotely.
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
